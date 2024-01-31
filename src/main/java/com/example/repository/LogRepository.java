d82e821834cf61e6e13aaef391c1e5ec21296ce7
AKAGIORI <123339911+AKAGIORI@users.noreply.github.com>
2024-02-01 08:27:21 +0900

    2/1

--- src/main/java/com/example/repository/LogRepository.java
+++ src/main/java/com/example/repository/LogRepository.java
@@ -1,5 +1,11 @@
 package com.example.repository;
 
-public class LogRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.entity.LogEntity;
+
+@Repository
+public interface LogRepository extends JpaRepository<LogEntity, Integer> {
 
 }
